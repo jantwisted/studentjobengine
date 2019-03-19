77d2315857a40524bdb98336994868f4c03987ea
jantwisted <janith@member.fsf.org>
2019-03-19 14:16:43 +0100

    log line added

--- main.go
+++ main.go
@@ -14,5 +14,6 @@ func main() {
     router := mux.NewRouter()
     router.HandleFunc("/jobs", ops.GetAllJobs).Methods("GET")
     router.HandleFunc("/jobs/add", ops.AddJob).Methods("POST")
+    ops.LogPrint("Listening port :8080")
     log.Fatal(http.ListenAndServe(":8080", router))
 }
