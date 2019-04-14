2b33d124099c83b04d6121d7901642ae36e9b4ae
Janith Perera <janith@member.fsf.org>
2019-04-14 02:08:13 +0200

    obselete functions removed

--- ops/restapi.go
+++ ops/restapi.go
@@ -8,36 +8,6 @@ import(
 	orm "../orm"
 )
 
-func GetAllJobs(w http.ResponseWriter, r *http.Request) {
-	pool := getRedisPool()
-	con := pool.Get()
-	defer con.Close()
-	keyarray, err := GetKeys(con, "JOB*")
-	if err != nil{
-		panic(err.Error())
-	}
-	job_array := JobStackArray{}
-	for _, keyvalue := range keyarray{
-		str, err := GetJsonValue(con, keyvalue)
-		LogError(err)
-		jobstackstr := GetJobStackStr(keyvalue, str)
-		job_array = AppendToJobStackArray(jobstackstr, job_array)
-	}
-	LogPrint("Sent JSON")
-	json.NewEncoder(w).Encode(job_array)
-}
-
-func GetJobStackStr(jobid string, info string)(string){
-	return "{\"id\":\""+jobid+"\",\"Info\":" +info+ "}"
-}
-
-func AppendToJobStackArray(jobstackstr string, job_array JobStackArray)(JobStackArray){
-	jobstack := JobStack{}
-	err := json.Unmarshal([]byte(jobstackstr), &jobstack)
-	LogError(err)
-	return append(job_array, jobstack)
-}
-
 
 
 func AddUser(w http.ResponseWriter, r *http.Request){
