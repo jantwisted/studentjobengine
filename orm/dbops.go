e8ce1eb9c836bd179056ca3b6a64b514e5c25af2
Janith Perera <janith@member.fsf.org>
2019-05-03 00:21:12 +0200

    bug fix

--- orm/dbops.go
+++ orm/dbops.go
@@ -126,7 +126,7 @@ func GetJobsFromDistance(distance int, db *gorm.DB){
 // get job ids based on the distance
 	var result []DistanceResult
 	db.Raw("SELECT X.id FROM ( SELECT id, ( 3959 * acos( cos( radians(47.085895) ) * cos( radians( cast(latitude as float) ) ) * cos( radians( cast(longtitude as float) ) - radians(17.900233) ) + sin( radians(47.085895) ) * sin( radians( cast(latitude as float) ) ) ) ) AS distance FROM jobs ) X WHERE X.distance < ? ORDER BY X.distance", distance).Scan(&result)
-	for r := range result {
+	for _, r := range result {
 		job := Search_Job_From_Idx(r.id, db)
 		fmt.Println(job.Title)
 	}
